039252f84052ab388a2b95bfe11e653810451725
Tincho228 <martin_quintero521@hotmail.com>
2025-12-12 18:00:14 -0300

    Creating groups of links on sidebar

--- viaje/resources/views/components/layouts/app/sidebar.blade.php
+++ viaje/resources/views/components/layouts/app/sidebar.blade.php
@@ -1,3 +1,17 @@
+@php 
+    $groups = [
+        'Platform' => [
+            [
+                'namespace' => 'Dashboard',
+                'icon' => 'home',
+                'url' => route('dashboard'),
+                'current' => request()->routeIs('dashboard')
+            ],
+        ]
+    ];
+
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
@@ -12,9 +26,13 @@
             </a>
 
             <flux:navlist variant="outline">
-                <flux:navlist.group :heading="__('Platform')" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
-                </flux:navlist.group>
+                @foreach ($groups as $group => $links)
+                    <flux:navlist.group :heading="$group" class="grid">
+                        @foreach ($links as $link)
+                            <flux:navlist.item :icon="$link['icon']" :href="$link['url']" :current="$link['current']" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                        @endforeach
+                    </flux:navlist.group>
+                @endforeach
             </flux:navlist>
 
             <flux:spacer />
