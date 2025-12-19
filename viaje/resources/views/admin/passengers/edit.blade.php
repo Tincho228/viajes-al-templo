59ac42df4ccd3bb2a4f18977f12c63efb25c4a26
Tincho228 <martin_quintero521@hotmail.com>
2025-12-19 08:37:46 -0300

    Fix:  Radio buttons on members

--- viaje/resources/views/admin/passengers/edit.blade.php
+++ viaje/resources/views/admin/passengers/edit.blade.php
@@ -14,7 +14,9 @@
         <form action="{{ route('admin.passengers.update', $passenger) }}" method="post" class=" space-y-4">
             @csrf
             @method('PUT')
+
                 <h1>Informacion Basica</h1>
+
                 <flux:input name="firstname"
                             value="{{old('firstname', $passenger->firstname)}}"
                             label="Nombre" 
@@ -38,11 +40,13 @@
                             label="DNI" 
                             placeholder="12.326.363" />
 
+               
+
                 <h1>Informacion de la iglesia</h1>
                 
                 <flux:radio.group label="Es miembro" name="is_member">
-                            <flux:radio value="1" label="Si" @checked(old('is_member', $passenger->is_member) == 1) />
-                            <flux:radio value="0" label="No" @checked(old('is_member', $passenger->is_member) == 0) />
+                            <flux:radio value="1" label="Si" :checked="(old('is_member', $passenger->is_member) == '1')"/>
+                            <flux:radio value="0" label="No" :checked="(old('is_member', $passenger->is_member) == '0')"/>
                 </flux:radio.group>
 
                 <flux:input name="membership"
@@ -55,6 +59,8 @@
                             <flux:radio value="1" label="Si" :checked="(old('is_endowed', $passenger->is_endowed) == '1')"/>
                             <flux:radio value="0" label="No" :checked="(old('is_endowed', $passenger->is_endowed) == '0')"/>
                 </flux:radio.group>
+                
+                
 
                 <h1>Perfil y edad</h1>
 
@@ -68,6 +74,8 @@
                             label="Fecha de nacimiento" 
                             name="birthdate" 
                             value="{{old('birthdate', $passenger->birthdate)}}"/>
+
+                
 
                 <h1>Informacion importante para padres y obispos</h1>
                 
